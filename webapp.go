02cff75a3521749e9fd253d433f6ea4c9ed05d8b
g00339839 <g00339839@gmit.ie>
2017-10-18 10:19:56 +0100

    Add a guess route

--- webapp.go
+++ webapp.go
@@ -21,11 +21,17 @@ func handler(w http.ResponseWriter, r *http.Request){
 	
 }
 
+func guessHandler(w http.ResponseWriter, r *http.Request){
 
+	var p = &Page{Title:"Guess a number between 1 and 20"}
+	t,_ := template.ParseFiles("guess.html")//parse html page
+	t.Execute(w,p)
+}
 
 func main(){
 
 
 	http.HandleFunc("/", handler)//handle any request 
+	http.HandleFunc("/guess",guessHandler)//handle /guess request
     http.ListenAndServe(":8080", nil)//listen to port 8080 in infinite loop
 }
