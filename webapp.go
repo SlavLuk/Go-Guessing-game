86c07d8f16851615d51babba88832b2d5b48f8d7
g00339839 <g00339839@gmit.ie>
2017-10-24 21:28:50 +0100

    Compare the cookie and the guess

--- webapp.go
+++ webapp.go
@@ -8,7 +8,8 @@ import (
 	"math/rand"
 	"log"
 	"strconv"
-	//"net/url"
+
+	
 
 )
 
@@ -17,8 +18,18 @@ type Page struct{
 	Message string
 	Guess int
 	Link string
-}
+	MessageWin string
 
+}
+func randomGen()string{
+
+	randNum := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(20)+1//generate random 1 <= number <= 20 
+	
+	randStr := strconv.Itoa(randNum)//convert random int to string
+
+	return randStr
+
+}
 func handler(w http.ResponseWriter, r *http.Request){
 
  p := &Page{Message:"Guessing game"}//assign title
@@ -33,10 +44,9 @@ func guessHandler(w http.ResponseWriter, r *http.Request){
 
 	var cookievalue string
 	var g int
+	var randStr string
 
-	randNum := rand.New(rand.NewSource(time.Now().UnixNano())).Intn(20)+1//generate random 1 <= number <= 20 
 	
-	randStr := strconv.Itoa(randNum)//convert random int to string
 
 	cookie,err:= r.Cookie("target")//get cookie named target
 
@@ -51,27 +61,51 @@ func guessHandler(w http.ResponseWriter, r *http.Request){
 	}
 
 
-	 if cookievalue == ""{//check if cookie is set
+	 if len(cookievalue) == 0{//check if cookie is set
+
+		randStr =randomGen()
 
 		cookieNew := http.Cookie{Name:"target",Value:randStr}//create cookie target to rand number
 	
 		http.SetCookie(w,&cookieNew)//set cookie
 	}
 
-	 urlVar :=r.URL.Query().Get("guess")//get guess value from input
+	 urlVal :=r.URL.Query().Get("guess")//get guess value from input
 
 
-	 if len(urlVar) == 0{//check if guess is set
+	 if len(urlVal) == 0{//check if guess is set
 
-		 urlVar = "0"//set to 0
+		 urlVal = "0"//set to 0
 	}
 
 	p := &Page{Message:"Guess a number between 1 and 20"}//create struct Page
 
+	g,_ = strconv.Atoi(urlVal)//convert string to int
+	cv,_ :=strconv.Atoi(cookievalue)
 
-	g,_ = strconv.Atoi(urlVar)//convert string to int
-	  
-	p.Guess = g//assign int to struct var guess
+ 	p.Guess = g//assign int to struct var guess
+
+	if cv > g && g!=0{
+		
+			p.MessageWin = "Your guess was too low"
+
+		}else if cv<g{
+
+			p.MessageWin = "Your guess was too high"
+
+		}else if g == cv && g!=0{
+
+			randStr = randomGen()
+			cookieNew := http.Cookie{Name:"target",Value:randStr}//create cookie target to rand number
+			
+			http.SetCookie(w,&cookieNew)//set cookie
+
+			p.MessageWin = "Congratulations You won !!!"
+			p.Link = "New Game"
+	
+		}
+
+			
 	
 	t,_ := template.ParseFiles("guess.tmpl")//parse html page
 	t.Execute(w,p)
